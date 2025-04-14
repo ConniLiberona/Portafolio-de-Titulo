d03e7c8da60e3f23d5a5499015040ab6c6ae733b
bastapiaux <164052290+bastapiaux@users.noreply.github.com>
2025-04-14 18:18:02 -0400

    Creando Login

---
+++ screens/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Login {
+    render() {
+        return (
+            <View>
+                <Text> textInCOmponent</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
