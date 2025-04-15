356b9a5006633ecc0c326fcccd39cddccd572655
bastapiaux <164052290+bastapiaux@users.noreply.github.com>
2025-04-15 00:18:29 -0400

    Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function Login(){
     return (
-        <View>
-            <Text> Login </Text>
+        <View style={styles.padre}>
+            <View>
+                <Image source={require('../assets/SAG.png')} style={styles.profile}/>
+            </View>
+
+            <View style={styles.tarjeta}>
+                <View style={styles.cajaTexto}>
+                    <TextInput placeholder='correo@gmail.com' style={{paddingHorizontal:15}}/>
+                </View>
+            </View>
+
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    padre:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        blackgroundColor:'white'
+    },
+    profile:{
+        width:100,
+        height:100,
+        borderRadius:50,
+        borderColor:'white'
+    },
+    tarjeta:{
+        margin:20,
+        backgroundColor:'white',
+        borderRadius:20,
+        width:'90%',
+        padding:20,
+        shadowColor:'#000',
+        shadowOffset:{
+            width:0,
+            height:2
+        },
+        shadowOpacity:0.25,
+        shadowRadius:4,
+        elevation:5,
+
+    }
+});
